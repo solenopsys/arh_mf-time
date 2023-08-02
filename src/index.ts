b46aca04c0d79143eeda64ec3af2563c1cc762c2
AlexStorm <info@alexstorm.ru>
2023-08-02 12:53:34 +0530

    entry refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export * from './app/entry.module';
-export * from './app/app.module';
+import {XsModule, XsModuleType} from "@solenopsys/fl-globals";
+import {RemoteEntryModule} from "./app/entry.module";
 
+export * from './app/entry.module';
+
+export const ENTRY: XsModule<RemoteEntryModule> = {
+    module: RemoteEntryModule,
+    type: XsModuleType.PLATFORM,
+};
